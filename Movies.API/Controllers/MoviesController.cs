03d742e609b4fc458302aea69902d78ad379938e
Francis Chung <francis.c.chung@gmail.com>
2025-03-10 11:37:13 +1100

    Adding Create functionality

--- Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
+using Movies.Application.Models;
+using Movies.Contracts.Responses;
 
 namespace Movies.API.Controllers
 {
@@ -18,7 +20,23 @@ namespace Movies.API.Controllers
         [HttpPost("movies")]
         public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
         {
-            return Ok(request);
+            var movie = new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Genres = request.Genres.ToList(),
+                YearOfRelease = request.YearOfRelease
+            };
+           var result = await _movieRepository.CreateAsync(movie);
+
+           var response = new MovieResponse
+           {
+               Id = movie.Id,
+               Title = movie.Title,
+               Genres = movie.Genres,
+               YearOfRelease = movie.YearOfRelease
+           }
+           return Ok(response);
         }
     }
 }
