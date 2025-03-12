6d2ab9110af26c5991d7c1ab263a06f91a6ecffc
Francis Chung <francis.c.chung@gmail.com>
2025-03-13 06:54:14 +1100

    New AddDatabase extension method

--- Movies.Application/ApplicationServiceCollectionExtensions.cs
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Database;
 using Movies.Application.Repositories;
 
 namespace Movies.Application
@@ -14,6 +15,11 @@ namespace Movies.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddSingleton<IMovieRepository, MovieRepository>();
+            return services;
+        }
+        public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IDbConnectionFactory>(_ => new NpgsqlConnectionFactory(connectionString));
             return services;
         }
     }
