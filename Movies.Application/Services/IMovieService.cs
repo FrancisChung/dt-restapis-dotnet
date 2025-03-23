2a2bc305aa18503294789f35a6d8ef4ff89af9c6
Francis Chung <francis.c.chung@gmail.com>
2025-03-24 10:55:47 +1100

    Turn tokens to defaults

--- Movies.Application/Services/IMovieService.cs
+++ Movies.Application/Services/IMovieService.cs
@@ -9,12 +9,12 @@ namespace Movies.Application.Services
 {
     public interface IMovieService
     {
-        Task<bool> CreateAsync(Movie movie, CancellationToken token);
-        Task<Movie?> GetByIdAsync(Guid id, CancellationToken token);
-        Task<Movie?> GetBySlugAsync(string slug, CancellationToken token);
-        Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token);
-        Task<Movie?> UpdateAsync(Movie movie, CancellationToken token);
-        Task<bool> DeleteByIdAsync(Guid id, CancellationToken token);
-        Task<bool> ExistsByIdAsync(Guid id, CancellationToken token);
+        Task<bool> CreateAsync(Movie movie, CancellationToken token = default);
+        Task<Movie?> GetByIdAsync(Guid id, CancellationToken token = default);
+        Task<Movie?> GetBySlugAsync(string slug, CancellationToken token = default);
+        Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default);
+        Task<Movie?> UpdateAsync(Movie movie, CancellationToken token = default);
+        Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
+        Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
     }
 }
