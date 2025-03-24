adaf36e17ca80882d7dea9f9191404b7a93e59d1
Francis Chung <francis.c.chung@gmail.com>
2025-03-24 11:00:39 +1100

    Cancellation Tokens added

--- Movies.Application/Services/MovieService.cs
+++ Movies.Application/Services/MovieService.cs
@@ -20,34 +20,34 @@ namespace Movies.Application.Services
             _movieValidator = movieValidator;
         }
 
-        public async Task<bool> CreateAsync(Movie movie)
+        public async Task<bool> CreateAsync(Movie movie, CancellationToken token = default)
         {
-            await _movieValidator.ValidateAndThrowAsync(movie);
-            return await _movieRepository.CreateAsync(movie);
+            await _movieValidator.ValidateAndThrowAsync(movie, token);
+            return await _movieRepository.CreateAsync(movie, token);
         }
-        public Task<bool> DeleteByIdAsync(Guid id)
+        public Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
         {
-            return _movieRepository.DeleteByIdAsync(id);
+            return _movieRepository.DeleteByIdAsync(id, token);
         }
-        public Task<bool> ExistsByIdAsync(Guid id)
+        public Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default)
         {
-            return _movieRepository.ExistsByIdAsync(id);
+            return _movieRepository.ExistsByIdAsync(id, token);
         }
-        public Task<IEnumerable<Movie>> GetAllAsync()
+        public Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default)
         {
-            return _movieRepository.GetAllAsync();
+            return _movieRepository.GetAllAsync(token);
         }
-        public Task<Movie?> GetBySlugAsync(string slug)
+        public Task<Movie?> GetBySlugAsync(string slug, CancellationToken token = default)
         {
-            return _movieRepository.GetBySlugAsync(slug);
+            return _movieRepository.GetBySlugAsync(slug, token);
         }
-        public Task<Movie?> GetByIdAsync(Guid id)
+        public Task<Movie?> GetByIdAsync(Guid id, CancellationToken token = default)
         {
-            return _movieRepository.GetByIdAsync(id);
+            return _movieRepository.GetByIdAsync(id, token);
         }
-        public async Task<Movie?> UpdateAsync(Movie movie)
+        public async Task<Movie?> UpdateAsync(Movie movie, CancellationToken token = default)
         {
-            await _movieValidator.ValidateAndThrowAsync(movie);
+            await _movieValidator.ValidateAndThrowAsync(movie, token);
             var movieExists = await _movieRepository.ExistsByIdAsync(movie.Id);
 
             if (!movieExists)
@@ -55,7 +55,7 @@ namespace Movies.Application.Services
                 return null;
             }
 
-            await _movieRepository.UpdateAsync(movie);
+            await _movieRepository.UpdateAsync(movie, token) ;
             return movie;
         }
     }
